317ff8ba346d0aa0cf8ed350b83792c67aefa1cb
Miranda W <36822462+mirandalwashburn@users.noreply.github.com>
2020-03-09 15:52:28 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script>
+
 $('.bonus').click(function() {
     $('.bonus__grid').toggleClass('bonus__show')
 })
-</script>
+
